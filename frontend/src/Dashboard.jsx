c8d9cf69082ed2c7f9b45abdc1c68f8e52529c03
PexArt-web <pexart74@gmail.com>
2025-04-27 07:01:51 +0100

    supabase

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,4 +1,29 @@
+import { useEffect } from "react";
+import supabase from "./supabase-client";
+
 function Dashboard() {
+  useEffect(() => {
+    const fetchMetrics = async () => {
+      try {
+        // const { data, error } =
+        const response = await supabase
+          .from("sales_deals")
+          .select(
+            `
+            name,
+           value,
+             `
+          )
+          .order("value", { ascending: false })
+          .limit(1);
+        console.log("Response:", response);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchMetrics();
+  }, []);
+
   return (
     <div className="dashboard-wrapper">
       <div className="chart-container">
